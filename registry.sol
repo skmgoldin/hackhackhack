34b994d6e39bd7df882163abb18c4a665580e10d
Mike Goldin <skmgoldin@gmail.com>
2015-06-25 15:13:52 +0000

    Recursive match checking.

--- registry.sol
+++ registry.sol
@@ -6,13 +6,25 @@ contract registry {
   // Adresses of interested contracts
   mapping(address => address) interestedParties; // Mapping of user addresses to their contracts
   address[MAX_USERS] users; // This can be iterated through to get the info in the mapping. (user addrs)
+  address[MAX_USERS] matchingUsers; // Users who match
   
   getMatches() { // Recursive function to find all matches for an event.
-    checkMatchHelper(msg.sender, users[0], 0);
+    checkMatchHelper(msg.sender, users[0], 0, 0);
   }
 
-  getMatchesHelper(address caller, address match, uint index) {
-    
+  getMatchesHelper(address caller, address match, uint usersIndex, uint matchIndex) {
+
+    if(checkMatch(caller, match)) {
+      matchingUsers[matchIndex] = match;
+      matchIndex++;
+    } 
+
+    usersIndex++;
+    if(usersIndex > MAX_USERS) {
+      return;
+    } else {
+      getMatchesHelper(caller, users[usersIndex], usersIndex, matchIndex);
+    }
 
   }
 
