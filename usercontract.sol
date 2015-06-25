e44967e30e64a7db9310051730e72df2509ff0dd
Mike Goldin <skmgoldin@gmail.com>
2015-06-25 21:33:26 +0000

    Deleted dead code.

--- usercontract.sol
+++ usercontract.sol
@@ -24,39 +24,4 @@ contract UserContract {
 
   }
 
-  function getMatches() {
-    //address[] matches = registry.call("getMatches");
-    //setNames(matches);
-  }
-
-  function setNames(address[] matches) {
-    uint index = 0; // THIS SHOULD BE INPUT RECURSIVELY
-    if(matches[index] != 0) {
-      address ratch = matches[index];  
-   /*   matchingParties = ratch.call("getName");  /* HAHAHAHAHA THIS IS ONLY GONNA RETURN ONE NAME
-                                                     matchingParties needs to be queried, if I can
-                                                     get one name out I'm happy. */
-    }
-  }
-
-  function getIdealTime() returns (uint) {
-    return idealTime;
-  }
-
-  function getBribeTime() returns (uint) {
-    return bribeTime;
-  }
-
-  function getBribePrice() returns (uint) { 
-    return bribePrice;
-  }
-
-  function getCharity() returns (uint) {
-    return charity;
-  }
-
-  function getName() returns (bytes32) {
-    return name;
-  }
-
 }
