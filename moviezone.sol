91b351537643c198af1c507fadb196943bb1e3d1
Mike Goldin <skmgoldin@gmail.com>
2015-06-25 17:51:56 +0000

    SOLOLITY

--- moviezone.sol
+++ moviezone.sol
@@ -1,13 +1,13 @@
 contract Registry {
 
-  bytes32 movieName = madMax;
-  uint MAX_USERS;
+  bytes32 movieName = "madMax";
+  uint MAX_USERS = 100;
   uint registeredUsers;
 
   mapping(address => User) userContracts; // Mapping of user addresses to their contracts
-  address[MAX_USERS] users;                      /* This can be iterated through to get the info in
+  address[100] users;                      /* This can be iterated through to get the info in
                                                     the mapping. (user addrs) */
-  address[MAX_USERS] matchingUsers;              // Users who match
+  address[100] matchingUsers;              // Users who match
   
 
   /* Constructor */
@@ -32,10 +32,10 @@ contract Registry {
     return matchingUsers;
   }
 
-  function getMatchesHelper(address caller, address match, uint usersIndex, uint matchIndex) {
+  function getMatchesHelper(address caller, address ratch, uint usersIndex, uint matchIndex) {
 
-    if(checkMatch(caller, userContracts[match])) {
-      matchingUsers[matchIndex] = match;
+    if(checkMatch(caller, userContracts[ratch])) {
+      matchingUsers[matchIndex] = ratch;
       matchIndex++;
     } 
 
@@ -48,9 +48,9 @@ contract Registry {
 
   }
 
-  function checkMatch(address caller, address match) returns (bool) {
+  function checkMatch(address caller, address ratch) returns (bool) {
     /* Ideal times match */
-    if(caller.call("getIdealTime") == match.call("getIdealTime")) {
+    if(caller.call("getIdealTime") == ratch.call("getIdealTime")) {
       return true;
     }
 
