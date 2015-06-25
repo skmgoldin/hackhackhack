2e757911c7f24a72b3ecdcdb2bccb9adc8c47899
Mike Goldin <skmgoldin@gmail.com>
2015-06-25 20:45:40 +0000

    User struct for comparison arch

--- registry.sol
+++ registry.sol
@@ -5,9 +5,23 @@ contract Registry {
   uint registeredUsers;
 
   mapping(address => address) userContracts; // Mapping of user addresses to their contracts
-  address[5] users;                        /* This can be iterated through to call methods
+  address[5] users;                          /* This can be iterated through to call methods
                                                 for all contracts in userContracts */
 
+  struct user {
+    address user;
+    uint idealTime;
+    uint bribeTime;
+    uint bribePrice;
+    uint charity;
+    bytes32 name;
+  }
+
+  user user0;
+  user user1;
+  user user2;
+  user user3;
+  user user4;
 
   /* Query variables for users who match an event wanted by a given address.
      Make sure to zero these out after use!! */
@@ -25,7 +39,10 @@ contract Registry {
   }
 
   function register(address userContract) {
+    if(registeredUsers == MAX_USERS) return;
+
     userContracts[msg.sender] = userContract;
+    users[registeredUsers] = msg.sender;
     registeredUsers++;
     makeOptimalMatch();
   }
